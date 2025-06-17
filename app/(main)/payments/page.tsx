dd9d124166f73ecef330818a6afc2170b3a145f6
zhanarys <zhanarys.mataev@delight.lv>
2025-06-17 13:18:56 +0600

    update

--- app/(main)/payments/page.tsx
+++ app/(main)/payments/page.tsx
@@ -5,6 +5,8 @@ import { Table } from "@/components/banner-prior/table/table"
 import { TableContext } from "@/components/banner-prior/table/table-provider";
 import { default_context } from "@/components/banner-prior/table/table-provider";
 import { useContext, useEffect } from "react";
+import useSWR from "swr";
+import { fetcher } from "@/fetcher";
 
 const labels = [
   {
@@ -23,12 +25,30 @@ const labels = [
 
 export default function PaymentsPage() {
   const { context, setContext } = useContext(TableContext);
+  const { data, isLoading } = useSWR({ url: "payment/list" }, fetcher)
+
+  useEffect(() => {
+    if (isLoading) {
+      setContext((prev) => ({
+        ...prev,
+        isLoading: true,
+      }));
+    }
+  }, [isLoading])
+
   useEffect(() => {
     setContext((prev) => ({
       ...prev,
       labels: labels,
     }));
   }, []);
+
+  useEffect(() => {
+    return () => {
+      setContext(default_context)
+    }
+  }, [])
+
   return (
     <div>
       <Header title={"Платежи"} subTitle={"Информация"} />
